9222fd8f0bc77a411834301c9dceff1837bb4105
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2015-05-22 21:35:12 +0100

    String writer complete + Makefile

--- src/assemble.c
+++ src/assemble.c
@@ -1,5 +1,57 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef int bool;
+#ifndef TRUE
+#define TRUE 1
+#endif
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+//TODO: Binary writer
+//TODO: Symbol Table
+//TODO: Tokeniser
+//TODO: Instructions
 
 int main(int argc, char **argv) {
+
+  if (argc != 3) {
+    perror("Incorrect Number of Arguements");
+    exit(EXIT_FAILURE);
+  }
+
+  FILE *input;
+  FILE *output;
+
+  if ((input = fopen(argv[1], "r")) == NULL) {
+    perror(argv[1]);
+    exit(EXIT_FAILURE);
+  }
+
+  if ((output = fopen(argv[2], "w")) == NULL) {
+    perror(argv[2]);
+    exit(EXIT_FAILURE);
+  }
+
+  printf("Correct Number of arguments\n");
+
+  printf("Compiles...\n");
+
+  printf("Now we write to file: ");
+
+  char *data = "This is the data we want to write to the file";
+
+  printf("%s\n", data);
+  //Write data to output file
+  if (fputs(data, output) <= 0) {
+    perror("Write to binary file failed.");
+    exit(EXIT_FAILURE);
+  }
+
+  fclose(input);
+  fclose(output);
+
   return EXIT_SUCCESS;
 }
