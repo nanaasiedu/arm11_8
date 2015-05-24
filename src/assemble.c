f9b691df32602a8ef02b92faa969656e862d753c
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2015-05-24 22:37:16 +0100

    Merge Conflicts

--- src/assemble.c
+++ src/assemble.c
@@ -3,9 +3,11 @@
 #include <string.h>
 #include "assemble.h"
 #include "symbolmap.h"
+#include <ctype.h>
 
 FILE *input = NULL, *output = NULL;
 SymbolTable *mnemonicTable = malloc(sizeof(SymbolTable));
+Tokens *tokens = NULL;
 
 int main(int argc, char **argv) {
 
@@ -13,9 +15,10 @@ int main(int argc, char **argv) {
     perror("Incorrect Number of Arguments");
     exit(EXIT_FAILURE);
   }
-  
+
   //Setup
   createMnemonicTable();
+  createTokenArray();
   setUpIO(argv[1], argv[2]);
 
   //SymbolTable *lblToAddr = malloc(sizeof(SymbolTable));
@@ -70,21 +73,47 @@ void resolveLabelAddresses(SymbolTable *map){
 #pragma mark - Helper Functions
 //TODO: Tokenise
 void tokenise() {
+  char line[512];
+  while (fgets(line, sizeof(line), input) != NULL) {
+    char *sep = " ,\n";
+    char *token;
+    for (token = strtok(line, sep); token; token(NULL, sep)) {
+      if (isLabel(token)) {
+        //Add LABEL
+      }
+      else if (isLiteral(token)) {
+        //Add LITERAL
+      }
+      else {
+        //Add OTHER
+      }
+    }
+    //Add NEW_LINE
+  }
+  //Add ENDFILE
+}
 
+bool isLabel(char *string) {
+  int length = strlen(string);
+  return isalpha(string[0]) && (string[length-1] == ':');
+}
+
+bool isLiteral(char *string) {
+  return string[0] == '#';
 }
 
 void parseProgram(SymbolTable *map) {}
 
 void createMnemonicTable() {
-  map_init(mnemonicTable); 
-  mnemonicTable->fullCapacity = 23; 
+  map_init(mnemonicTable);
+  mnemonicTable->fullCapacity = 23;
   mnemonicTable->size = 23;
   mnemonicTable->keys = {"add","sub","rsb","and","eor",
                          "orr","mov","tst","teq","cmp",
-                         "mul","mla", 
+                         "mul","mla",
                          "ldr","str",
                          "beq","bne", "bge","blt","bgt","ble","b",
-                         "lsl","andeq"}; 
+                         "lsl","andeq"};
   mnemonicTable->values = {4,2,3,0,1,
                            12,13,8,9,10,
                            0,1,
@@ -92,8 +121,9 @@ void createMnemonicTable() {
                            0,1,10,11,12,13,14};
 }
 
-
-
-
-
-
+void createTokenArray() {
+  tokens = malloc(sizeof(Tokens));
+  tokens->size = 0;
+  tokens->fullCapacity = 100;
+  tokens->tokens = malloc(sizeof(Token) * tokens->fullCapacity);
+}
--- src/assemble.h
+++ src/assemble.h
@@ -16,12 +16,17 @@ typedef int bool;
 #define WORD_SIZE 4
 #endif
 
-typedef enum {label,literal,other,newline,endfile} TokenType;
+typedef enum {LABEL,LITERAL,OTHER,NEWLINE,ENDFILE} TokenType;
 
 typedef struct {
   char *value;
   TokenType type
 } Token;
+
+#ifndef NOT_FOUND
+#define NOT_FOUND -1
+#endif
+
 
 //IO
 void setUpIO(char *in, char *out);
@@ -31,5 +36,7 @@ void parseProgram(SymbolTable *map);
 //Helper
 void tokenise();
 void createMnemonicTable();
+bool isLabel(char *string);
+bool isLiteral(char *string);
 
 #endif /* end of include guard: ASSEMBLE */
