af76d07516246369c890218e9f7c8ef6d3c4ceda
fre14 <femile@blueyonder.co.uk>
2015-05-25 16:25:30 +0100

    All up to ParseInstruction()
    parseProgram
    parseLine
    parseInstruction
    helper methods
    initialised mnemonics symbol table

--- src/assemble.c
+++ src/assemble.c
@@ -4,8 +4,25 @@
 #include "assemble.h"
 #include "symbolmap.h"
 
+
 FILE *input = NULL, *output = NULL;
-SymbolTable *mnemonicTable = malloc(sizeof(SymbolTable));
+char *mnemonicStrings[23] = {
+  "add","sub","rsb","and","eor","orr","mov","tst","teq","cmp",
+  "mul","mla", 
+  "ldr","str",
+  "beq","bne", "bge","blt","bgt","ble","b",
+  "lsl","andeq"
+};
+
+int mnemonicInts[23] = {
+  4,2,3,0,1,12,13,8,9,10,
+  0,1,
+  0,0,
+  0,1,10,11,12,13,14
+};                               
+
+SymbolTable mnemonicTable = {23,23,mnemonicStrings,mnemonicInts}; 
+Token *tokens;
 
 int main(int argc, char **argv) {
 
@@ -15,9 +32,8 @@ int main(int argc, char **argv) {
   }
   
   //Setup
-  createMnemonicTable();
   setUpIO(argv[1], argv[2]);
-
+ 
   //SymbolTable *lblToAddr = malloc(sizeof(SymbolTable));
   tokenise();
   //resolveLabelAddresses(lblToAddr);
@@ -26,7 +42,6 @@ int main(int argc, char **argv) {
 
   fclose(input);
   fclose(output);
-  map_free(mnemonicTable);
 
   return EXIT_SUCCESS;
 }
@@ -67,30 +82,85 @@ void resolveLabelAddresses(SymbolTable *map){
   // map_print(map);
 }
 
+void parseProgram(SymbolTable *map) {
+  int i = 0;
+  while (tokens[i].type != ENDFILE) {
+    parseLine(tokens+i);
+    i++;
+  }
+}
+
+void parseLine(Token *token) {
+  if (token->type == OTHER) {
+    parseInstruction(token);
+  } 
+}
+
+
+void parseInstruction(Token *token) {
+  switch(index_of(token)) {
+    case ADD: parseAdd();
+    break;
+    case SUB: parseSub()
+    break;
+    case RSB: parseRsb()
+    break;
+    case AND: parseAnd()
+    break;
+    case EOR: parseEor()
+    break;
+    case ORR: parseOrr(); 
+    break;
+    case MOV: parseMov();
+    break;
+    case TST: parseTst();
+    break;
+    case TEQ: parseTeq();
+    break;
+    case CMP: parseCmp();
+    break;
+    case MUL: parseMul();
+    break;
+    case MLA: parseMla();
+    break;
+    case BEQ: parseBeq();
+    break;
+    case BNE: parseBne();
+    break;
+    case BGE: parseBge();
+    break;
+    case BLT: parseBlt();
+    break;
+    case BGT: parseBgt();
+    break;
+    case BLE: parseBle();
+    break;
+    case B: parseB();
+    break;
+    case LSL: parseLsl();
+    break;
+    case ANDEQ: parseAndeq();
+    break;
+
+  } 
+
+}
+
 #pragma mark - Helper Functions
 //TODO: Tokenise
 void tokenise() {
 
 }
 
-void parseProgram(SymbolTable *map) {}
-
-void createMnemonicTable() {
-  map_init(mnemonicTable); 
-  mnemonicTable->fullCapacity = 23; 
-  mnemonicTable->size = 23;
-  mnemonicTable->keys = {"add","sub","rsb","and","eor",
-                         "orr","mov","tst","teq","cmp",
-                         "mul","mla", 
-                         "ldr","str",
-                         "beq","bne", "bge","blt","bgt","ble","b",
-                         "lsl","andeq"}; 
-  mnemonicTable->values = {4,2,3,0,1,
-                           12,13,8,9,10,
-                           0,1,
-                           0,0,
-                           0,1,10,11,12,13,14};
+int index_of(Token *token) {
+  int i = 0;
+  while (strcmp(token->value,mnemonicStrings[i]) != 0) {
+    i++;
+  } 
+  return i; 
 }
+
+
 
 
 
--- src/assemble.h
+++ src/assemble.h
@@ -16,11 +16,23 @@ typedef int bool;
 #define WORD_SIZE 4
 #endif
 
-typedef enum {label,literal,other,newline,endfile} TokenType;
+#ifndef N_MNEMONICS
+#define N_MNEMONICS 23
+#endif
+
+typedef enum {LABEL,LITERAL,OTHER,NEWLINE,ENDFILE} TokenType;
+
+typedef enum {
+    ADD,SUB,RSB,AND,EOR,ORR,MOV,TST,TEQ,CMP,
+    MUL,MLA,
+    LDR,STR,
+    BEQ,BNE,BGE,BLT,BGT,BLE,B,
+    LSL,ANDEQ
+} Mnemonics;
 
 typedef struct {
   char *value;
-  TokenType type
+  TokenType type;
 } Token;
 
 //IO
@@ -28,8 +40,32 @@ void setUpIO(char *in, char *out);
 //Compile
 void resolveLabelAddresses(SymbolTable *map);
 void parseProgram(SymbolTable *map);
+void parseLine(Token *token);
+void parseInstruction(Token *token);
+//ParseInstructions
+void parseAdd();
+void parseSub();
+void parseRsb();
+void parseAnd();
+void parseEor();
+void parseOrr();
+void parseMov();
+void parseTst();
+void parseTeq();
+void parseCmp();
+void parseMul();
+void parseMla();
+void parseBeq();
+void parseBne();
+void parseBge();
+void parseBlt();
+void parseBgt();
+void parseBle();
+void parseB();
+void parseLsl();
+void parseAndeq();
 //Helper
 void tokenise();
-void createMnemonicTable();
+int index_of(Token *token);
 
 #endif /* end of include guard: ASSEMBLE */
