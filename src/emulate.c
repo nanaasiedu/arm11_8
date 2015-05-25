aa2262cda2e5a1d2ccbd7d29a11229d9586ba931
Nana <na1814@ic.ac.uk>
2015-05-25 12:05:13 +0100

    Added cleanup function

--- src/emulate.c
+++ src/emulate.c
@@ -1,11 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h> // reg Uint32
+
+uint8_t mem[65536] = calloc(65536); // RAM
 
 int main(int argc, char **argv) {
   if (argc != 1) {
     printf("Incorrect number of arguments");
     exit(EXIT_FAILURE);
   }
-  
+
+  cleanup()
   return EXIT_SUCCESS;
+}
+
+void cleanup(void) {
+// cleanup frees up memory used within the program when program shuts down
+  free(mem);
 }
