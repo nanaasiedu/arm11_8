357280cc5abb957a744f90bbef9f877fe72e2d95
Nana <na1814@ic.ac.uk>
2015-05-25 10:19:16 +0100

    edited emulate
    added small if statement to main

--- src/emulate.c
+++ src/emulate.c
@@ -1,5 +1,11 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char **argv) {
+  if (argc != 1) {
+    printf("Incorrect number of arguments");
+    exit(EXIT_FAILURE);
+  }
+  
   return EXIT_SUCCESS;
 }
