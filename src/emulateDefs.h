9bf7e7750c70cc69dde3363ababfaa778bae0069
elyasaddo <les_a2do@yahoo.co.uk>
2015-05-25 12:50:54 +0100

    Emergency commit

--- src/emulateDefs.h
+++ src/emulateDefs.h
@@ -23,29 +23,6 @@ typedef struct {
   int32_t operandOffset;
 } DecodedInst;
 
-struct regFile {
-  uint32_t reg[17]; // registers 0-12 are general purpose.
-  uint32_t *SP = &reg[13];
-  uint32_t *LR = &reg[14];
-  uint32_t *PC = &reg[15];
-  uint32_t *CPSR = &reg[16];
-}
 
-// #ifndef WORD_SIZE
-// #define WORD_SIZE 4
-// #endif
-
-/*
-//IO
-void setUpIO(char *in, char *out);
-void outputData(char *data);
-//Compile
-char* compile();
-void firstPass(SymbolTable *map);
-void secondPass(SymbolTable *map);
-//Helper
-bool hasLabel(char *str);
-bool isBlankLine(char *str);
-*/
 
 #endif /* end of include guard: EMULATEDEFS */
