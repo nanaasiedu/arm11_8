49dc91ba47f2e1e8542a486a71b96d7b52f02ae0
elyasaddo <les_a2do@yahoo.co.uk>
2015-05-25 13:01:47 +0100

    Attempt to fix merge with 'added register file' commit

--- src/emulateDefs.h
+++ src/emulateDefs.h
@@ -23,6 +23,12 @@ typedef struct {
   int32_t operandOffset;
 } DecodedInst;
 
-
+struct regFile {
+  uint32_t reg[17]; // registers 0-12 are general purpose.
+  uint32_t *SP = &reg[13];
+  uint32_t *LR = &reg[14];
+  uint32_t *PC = &reg[15];
+  uint32_t *CPSR = &reg[16];
+}
 
 #endif /* end of include guard: EMULATEDEFS */
