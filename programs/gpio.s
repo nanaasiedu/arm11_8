4218423d367651b549f8e450955260677fdb7679
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2015-06-10 16:55:45 +0100

    Tokenise Comments

--- programs/gpio.s
+++ programs/gpio.s
@@ -1,14 +1,14 @@
 ldr r0, =0x20200000
 mov r2, #1
 lsl r2, #18
-str r2, [r0, #4]
+str r2, [r0, #4] ; comment
 
 mov r1, #1
 lsl r1, #16
 
 mov r2, #1
 
-loop:
+loop: ; loop
 
 cmp r2, #0
 bne else
@@ -17,12 +17,12 @@ b endif
 else:
 str r1, [r0, #40]
 endif:
-eor r2, r2, #1
-
+eor r2, r2, #1 ;xor
+;
 mov r3, #0x2e0000
 delay:
 sub r3, r3, #1
 cmp r3, #0
 bne delay
-
-b loop
+; emptyline
+b loop ; endloop
